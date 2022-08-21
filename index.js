f522babafece34e5d4e48ef5b033969568158208
Mutuma Gitonga <mgitonga150@gmail.com>
2022-08-21 05:55:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,36 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  
+  const result = tutorials.map((sentence) => {
+    
+      const wordsArray = sentence.split(" ");
+      
+      const titleCasedWord = wordsArray.map((word) => {
+        
+        const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+        // RETURN STRINGS WITH NON-CAPITALIZED FIRST CHAR AFTER CAPITALIZING THEM 
+        if(!(word.charAt(0).toUpperCase() === word.charAt(0)) && !(word.charAt(0) !== word.charAt(0).toLowerCase())){
+          return `${word.charAt(0).toUpperCase()}${word.substring(1,word.length)}`;
+        }  
+          // USING REGEX, RETURN NON-ALPHANUMERIC STRINGS AND EXCLUDE THOSE WITH ALPHANUMERIC CHARACTERS. OFCOURSE TITLE CASE UNNECESSARY
+          else if(specialChars.test(word) && !/[a-zA-Z]/.test(word)) {
+            return `${word}`;
+          }
+            // RETURN WORDS WITH CAPITALIZED FIRST CHAR (CAPITALIZATION UNNECESSARY) 
+            else {
+              return `${word}`;
+          }
+        
+      });
+    
+      return titleCasedWord.join(" ");
+     
+  });
+
+  return result;
 }
+
+// console.log(titleCased());
+
